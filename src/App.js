a54187df96dbd7c2ec20315ee41cb25be88203be
unknown <flowerrosa76@gmail.com>
2023-01-22 10:52:49 +0900

    first v

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import {useEffect, useState} from"react";
+import './App.css';
+import MyRecipesComponent from "./MyRecipesComponent";
+import image from './img.jpg';
+
+
+function App(){
+
+  const My_ID = "801c72eb";
+  const My_KEY = "29c30b95fab850060525f868bb388667";
+  const [searchInput,setSearchInput] = useState('');
+  const [getRecipes,setGetRecipes] = useState([]);
+  const[wordSubmitted,setWordSubmitted] =useState('avocado');
+
+  useEffect(()=>{
+    const getApi = async()=>{
+      const response = await fetch (`https://api.edamam.com/api/recipes/v2?type=public&q=${wordSubmitted}&app_id=${My_ID}&app_key=${My_KEY}`);
+      const data = await response.json();
+      console.log(data.hits);
+      setGetRecipes(data.hits);
+    }
+    getApi();
+  },[wordSubmitted]);
+
+  const myRecipeSearch =(e)=>{
+    console.log(e.target.value);
+    setSearchInput(e.target.value);
+  }
+  const finalSearch =(e)=>{
+    e.preventDefault();
+    setWordSubmitted(searchInput);
+  }
+ 
+  return(
+    <div>
+        <div className ="container" >
+      <img  className="main_img" src = {image} alt="food"/>  
+      <h1>Find a recipe</h1>  
     </div>
-  );
+
+    <div className="container">
+      
+      <form onSubmit={finalSearch}>
+        <input onChange={myRecipeSearch} value={searchInput} className="search" placeholder="Search recipes ...."></input>
+      </form>
+    </div>
+  <div>
+     {getRecipes.map((element,index )=>(
+      <MyRecipesComponent key={index}
+      propsLabel = {element.recipe.label}
+      propsImage = {element.recipe.image}
+      propsCalories = {element.recipe.calories}
+      propsCuisineType = {element.recipe.cuisineType}
+      propsIngredients = {element.recipe.ingredientLines}         
+      />
+     ))}
+    
+    </div>
+    </div>
+  )
 }
 
 export default App;
